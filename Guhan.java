d56efee924ce0bedaebef7607fa22ce4cbe738b8
Guhan M <116095658+guhan0000@users.noreply.github.com>
2025-06-05 19:33:41 +0530

    Add files via upload

---
+++ Guhan.java
@@ -0,0 +1,10 @@
+public class Guhan
+{
+	public static void main(String args[])
+	{
+		int age=22;
+		String desg ="Software Developer";
+		String name ="Guhan";
+		System.out.println(name+" is a "+age+" years old "+desg+".");
+	}
+}
